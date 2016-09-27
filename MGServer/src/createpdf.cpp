80491b79c58e1fa3e5d7ba43678a8e8060c451d0
Caleg74 <gabriele.caletti@gmail.com>
2016-09-27 18:35:57 +0200

    Html example for pdf creation

--- MGServer/src/createpdf.cpp
+++ MGServer/src/createpdf.cpp
@@ -1,32 +1,114 @@
 #include "createpdf.h"
-#include <QPrinter>
-#include <QTextStream>
 #include <QFile>
 #include <QDebug>
+#include <QTextCursor>
 
 CreatePdf::CreatePdf()
  : QObject()
- , m_doc()
+// , m_doc()
+ , m_printer(QPrinter::PrinterResolution)
 {
 
 }
 
 void CreatePdf::Print()
 {
-    QString strFileName = "../MGServer/pdf/MemorialGander2016.pdf";
-    QPrinter printer(QPrinter::PrinterResolution);
-    printer.setOutputFormat(QPrinter::PdfFormat);
-    printer.setPaperSize(QPrinter::A4);
-    printer.setOutputFileName(strFileName);
+    //-------
+//    QPrinter printer;
+//    printer.setOutputFormat(QPrinter::PdfFormat);
+//    printer.setOutputFileName("/foobar/nonwritable.pdf");
+//    QPainter painter;
+//    if (! painter.begin(&printer)) { // failed to open file
+//        qWarning("failed to open file, is it writable?");
+//        return 1;
+//    }
+//    painter.drawText(10, 10, "Test");
+//    if (! printer.newPage()) {
+//        qWarning("failed in flushing page to disk, disk full?");
+//        return 1;
+//    }
+//    painter.drawText(10, 10, "Test 2");
+//    painter.end();
+    //-----------
 
+    QString strFileName = "../MGServer/pdf/MemorialGander2016.pdf";
+    m_printer.setOutputFormat(QPrinter::PdfFormat);
+    m_printer.setPaperSize(QPrinter::A4);
+    m_printer.setOutputFileName(strFileName);
+
+    if (! m_painter.begin(&m_printer)) { // failed to open file
+        qWarning("failed to open file, is it writable?");
+        return;
+    }
+
+   PrintMenAllround(false);
+
+    if (! m_printer.newPage()) {
+        qWarning("failed in flushing page to disk, disk full?");
+    }
+
+    PrintWomenAllround(false);
+
+//    PrintWomenAllround(false);
+//    m_printer.newPage();
+
+//    PrintMenCitycup(false);
+//    m_printer.newPage();
+
+//    PrintWomenCitycup(false);
+
+//    m_doc.setHtml(m_textStream.readAll());
+//    m_doc.setPageSize(m_printer.pageRect().size()); // This is necessary if you want to hide the page number
+
+    m_painter.end();
+//    m_doc.print(&m_printer);
+}
+
+void CreatePdf::PrintMenAllround(bool p_bHFImages)
+{
     QTextDocument doc;
-    QFile file("../MGServer/pdf/TestHtml.html");
+
+    QFile file("../MGServer/pdf/TestHtmlMen.html");
     if(!file.open(QIODevice::ReadOnly)) {
         qDebug() << "Unable to open file " << file.errorString();
     }
-    QTextStream in(&file);
+    QTextStream strLocalText(&file);
+    QTextCursor cur(&doc);
+//    cur.movePosition(QTextCursor::Start);
+//    cur.insertText("Ciao!");
+    cur.insertHtml(strLocalText.readAll());
 
-    m_doc.setHtml(in.readAll());
-    m_doc.setPageSize(printer.pageRect().size()); // This is necessary if you want to hide the page number
-    m_doc.print(&printer);
+    doc.drawContents(&m_painter);
+    file.close();
+}
+
+void CreatePdf::PrintWomenAllround(bool p_bHFImages)
+{
+    QTextDocument doc;
+    QFile file("../MGServer/pdf/TestHtmlWomen.html");
+    if(!file.open(QIODevice::ReadOnly)) {
+        qDebug() << "Unable to open file " << file.errorString();
+    }
+    QTextStream strLocalText(&file);
+
+    QTextCursor cur(&doc);
+//    cur.movePosition(QTextCursor::Start);
+//    cur.insertText("Ciao!");
+    cur.insertHtml(strLocalText.readAll());
+
+
+    doc.drawContents(&m_painter);
+
+    file.close();
+}
+
+void CreatePdf::PrintMenCitycup(bool p_bHFImages)
+{
+
+}
+
+
+void CreatePdf::PrintWomenCitycup(bool p_bHFImages)
+{
+
 }
--- MGServer/src/createpdf.h
+++ MGServer/src/createpdf.h
@@ -2,6 +2,9 @@
 #define CREATEPDF_H
 
 #include <QTextDocument>
+#include <QPrinter>
+#include <QTextStream>
+#include <QPainter>
 
 class CreatePdf : public QObject
 {
@@ -14,7 +17,32 @@ public slots:
 
 private:
 
-    QTextDocument m_doc;
+    /** Creates a pdf for the Men's allround ranking.
+     * @param p_bHFImages true=with Header/Footer sponsor images
+     */
+    void PrintMenAllround(bool p_bHFImages);
+
+    /** Creates a pdf for the Women's allround ranking.
+     * @param p_bHFImages true=with Header/Footer sponsor images
+     */
+    void PrintWomenAllround(bool p_bHFImages);
+
+    /** Creates a pdf for the Men's citycup ranking.
+     * @param p_bHFImages true=with Header/Footer sponsor images
+     */
+    void PrintMenCitycup(bool p_bHFImages);
+
+    /** Creates a pdf for the Women's citycup ranking.
+     * @param p_bHFImages true=with Header/Footer sponsor images
+     */
+    void PrintWomenCitycup(bool p_bHFImages);
+
+//    QTextDocument m_doc;
+    QTextStream m_textStream;
+
+    QPainter m_painter;
+
+    QPrinter m_printer;
 };
 
 #endif // CREATEPDF_H
